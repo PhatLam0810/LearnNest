d37b5167535bca75e3c5d1796949aa35691388d8
PhatLT <phatlam@ntada.vb>
2025-02-26 15:56:51 +0700

    config fire base

--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDWnIfFfq5FHHWyGkZN4_xfWrnpaYtmauw',
-  authDomain: 'yogapalooza-f0206.firebaseapp.com',
-  projectId: 'yogapalooza-f0206',
-  storageBucket: 'yogapalooza-f0206.firebasestorage.app',
-  messagingSenderId: '994068721491',
-  appId: '1:994068721491:web:43f795c99d3e684ba3bba6',
-  measurementId: 'G-YQJ53QRBX6',
+  apiKey: 'AIzaSyBFHFIQ_0RI4B_Tfkax9n5f-1EFiN6f830',
+  authDomain: 'my-project-c7f44.firebaseapp.com',
+  projectId: 'my-project-c7f44',
+  storageBucket: 'my-project-c7f44.firebasestorage.app',
+  messagingSenderId: '806909046522',
+  appId: '1:806909046522:web:ad7d5f744596e3d61ba343',
+  measurementId: 'G-PLQE90PFZ6',
 };
 
 const app = initializeApp(firebaseConfig);
