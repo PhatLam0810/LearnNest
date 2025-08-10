baff22e022a95347463a094591a85c3fa42c9c0c
PhatLT <lamtanphat56@gmail.com>
2025-08-10 19:39:49 +0700

    fix firebase

--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBFHFIQ_0RI4B_Tfkax9n5f-1EFiN6f830',
-  authDomain: 'my-project-c7f44.firebaseapp.com',
-  projectId: 'my-project-c7f44',
-  storageBucket: 'my-project-c7f44.firebasestorage.app',
-  messagingSenderId: '806909046522',
-  appId: '1:806909046522:web:ad7d5f744596e3d61ba343',
-  measurementId: 'G-PLQE90PFZ6',
+  apiKey: 'AIzaSyDSCwwzaHgb_lt2e2JlvEpIf07SfM5AE_U',
+  authDomain: 'learnnest-6aff3.firebaseapp.com',
+  projectId: 'learnnest-6aff3',
+  storageBucket: 'learnnest-6aff3.firebasestorage.app',
+  messagingSenderId: '707028095947',
+  appId: '1:707028095947:web:7c15c7021d6996dd485c4e',
+  measurementId: 'G-KPB0HVK16C',
 };
 
 const app = initializeApp(firebaseConfig);
