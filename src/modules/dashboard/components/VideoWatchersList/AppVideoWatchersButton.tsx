f84eaef126899e6454eaff02213d6c7da4aeac1e
MinhTH <minhnhattuho456@gmail.com>
2025-12-12 12:15:23 +0700

    fix trackingvideo

---
+++ src/modules/dashboard/components/VideoWatchersList/AppVideoWatchersButton.tsx
@@ -0,0 +1,39 @@
+// components/AppVideoWatchersButton.tsx
+'use client';
+
+import React from 'react';
+import { Button, Tooltip } from 'antd';
+import { EyeOutlined } from '@ant-design/icons';
+
+interface AppVideoWatchersButtonProps {
+  subLessonId: string;
+  subLessonTitle: string;
+  disabled?: boolean;
+  onClick?: (e?: any) => void;
+}
+
+const AppVideoWatchersButton: React.FC<AppVideoWatchersButtonProps> = ({
+  subLessonId,
+  subLessonTitle,
+  disabled = false,
+  onClick,
+}) => {
+  return (
+    <Tooltip title="View users who watched this video">
+      <Button
+        type="text"
+        icon={<EyeOutlined />}
+        size="small"
+        onClick={onClick}
+        disabled={disabled}
+        style={{
+          color: disabled ? '#ccc' : '#1890ff',
+          border: '1px solid #d9d9d9',
+          borderRadius: 4,
+        }}
+      />
+    </Tooltip>
+  );
+};
+
+export default AppVideoWatchersButton;
