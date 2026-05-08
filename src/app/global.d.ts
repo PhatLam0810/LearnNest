d994683d7aa7b6b9835ff95799efc8b3ebb2cf49
PhatLT <lamtanphat56@gmail.com>
2026-05-08 16:56:11 +0700

    fix env file

--- src/app/global.d.ts
+++ src/app/global.d.ts
@@ -3,3 +3,27 @@ declare module '*.scss' {
   export default content;
 }
 declare module '*.css';
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.webp' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
